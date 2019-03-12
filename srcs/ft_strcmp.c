a1e899e0e3cabbf75f3b6d630519e94b5d97022e
Kai Chen <kchen2@e1z2r3p6.42.us.org>
2019-03-11 20:00:10 -0700

    fix or strcmp

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kchen2 <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 13:44:56 by kchen2            #+#    #+#             */
-/*   Updated: 2019/03/11 19:44:28 by kchen2           ###   ########.fr       */
+/*   Updated: 2019/03/11 19:58:44 by kchen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		ft_strcmp(const char *s1, const char *s2)
 	int		i;
 
 	i = 0;
-	while (s1[i] == s2[i])
+	while (s1[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }

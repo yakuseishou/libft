e4139359d762661c9a31f190359ff8c6d09fd6c1
Kai Chen <kchen2@e1z2r3p6.42.us.org>
2019-03-11 20:14:48 -0700

    strncmp

---
+++ srcs/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchen2 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/11 14:20:50 by kchen2            #+#    #+#             */
+/*   Updated: 2019/03/11 19:49:48 by kchen2           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (!n)
+		return (0);
+	i = 0;
+	while (i < n && (s1[i] && s2[i]) && s1[i] == s2[i])
+		i++;
+//	if (i == n)
+//		return ((unsigned char)s1[i - 1] - (unsigned char)s2[i - 1]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}

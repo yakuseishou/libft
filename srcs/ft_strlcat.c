746bae7f649b5d501aabed2766d95965313525f9
Kai Chen <kchen2@e1z2r2p7.42.us.org>
2019-11-15 17:13:45 -0800

    small optimaz

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchen2 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: kchen2 <kchen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 14:10:28 by kchen2            #+#    #+#             */
-/*   Updated: 2019/02/18 16:00:40 by kchen2           ###   ########.fr       */
+/*   Updated: 2019/11/15 17:13:06 by kchen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,50 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
-	size_t	i;
-	size_t	j;
-	size_t	a;
+	size_t	dl;
+	const char *s;
+	size_t	nt;
+	char	*d;
 
-	i = ft_strlen(dst);
-	j = ft_strlen(src);
-	if (n < i)
-		return (n + j);
-	a = 0;
-	while (src[a] && (i + a + 1) < n)
+	d = dst;
+	s = src;
+	nt = n;
+	while (nt-- != 0 && *d != '\0')
+		d++;
+	dl = d - dst;
+	nt = n - dl;
+	if (nt == 0)
+		return (dl + ft_strlen(s));
+	while (*s != '\0')
 	{
-		dst[i + a] = src[a];
-		a++;
+		if (nt > 1)
+		{
+			*d = *s;
+			d++;
+			nt--;
+		}
+		s++;
 	}
-	dst[i + a] = 0;
-	return (i + j);
+	*d = '\0';
+	return (dl + (s - src));
 }
+
+// size_t	ft_strlcat(char *dst, const char *src, size_t n)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	a;
+
+// 	i = ft_strlen(dst);
+// 	j = ft_strlen(src);
+// 	if (n < i)
+// 		return (n + j);
+// 	a = 0;
+// 	while (src[a] && (i + a + 1) < n)
+// 	{
+// 		dst[i + a] = src[a];
+// 		a++;
+// 	}
+// 	dst[i + a] = 0;
+// 	return (i + j);
+// }
